00e23edfb93e87c6049a46bf13c00200cf4c9724
Shawn Thorne <shawn.thorne9@gmail.com>
2023-04-24 13:18:55 -0600

    working stuff

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,12 +14,13 @@ const firebaseConfig = {
   storageBucket: "geotodosapp.appspot.com",
   messagingSenderId: "716093370179",
   appId: "1:716093370179:web:9c908a801992208c4d89f6",
-  measurementId: "G-4FB36K1XMC"
+  measurementId: "G-4FB36K1XMC",
+  databaseURL: "https://geotodosapp-default-rtdb.firebaseio.com/"
 };
 
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+// export const db = getFirestore(app);
 export const rtdb = getDatabase(app);
