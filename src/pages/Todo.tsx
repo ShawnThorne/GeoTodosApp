3db8b0e51e51a9727a3511863cf971d5d53fd7ae
luke Shumway <a02268065@aggies.usu.edu>
2023-05-02 13:44:44 -0600

    added functionality to todos detail page. can now edit or delete the todo

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -2,19 +2,22 @@ import { useContext, useEffect, useState } from "react";
 import { rtdb } from "../lib/firebase";
 import UserContext from "../context/user";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom"
-import { set, get, ref, child, push, update } from "firebase/database";
+import { set, get, ref, child, push, update, remove } from "firebase/database";
 
 export const Todo = () => {
     const params = useParams();
     const user = useContext(UserContext);
     const navigate = useNavigate();
-    const [message, setMessage] = useState();
-    const [isComplete, setComplete] = useState();
+    const [message, setMessage] = useState("");
+    const [oldMessage, setOldMessage] = useState("");
+    const [isComplete, setComplete] = useState(false);
+    const [edit, setEdit] = useState(false);
 
     useEffect(()=>{
         get(child(ref(rtdb), `users/${user?.uid}/locations/${params.location}/todos/${params.message}`))
         .then(snapshot =>{
             setMessage(snapshot.val()['message'])
+            setOldMessage(snapshot.val()['message'])
             setComplete(snapshot.val()['isComplete'])
         })
     },[])
@@ -27,11 +30,43 @@ export const Todo = () => {
             navigate('/dashboard');
         })
     }
+
+    function saveTask(){
+        remove(ref(rtdb,`/users/${user?.uid}/locations/${params.location}/todos/${oldMessage}`))
+        set(ref(rtdb,`/users/${user?.uid}/locations/${params.location}/todos/${message}`),{
+            isComplete: isComplete,
+            message: message
+        }).then(()=>{
+            setEdit(false);
+            setOldMessage(message);
+        })
+    }
+    
+    function deleteTask(){
+        remove(ref(rtdb,`/users/${user?.uid}/locations/${params.location}/todos/${message}`)).then(()=>{
+            navigate('/dashboard');
+        })
+    }
+
     return (
         <div>
-            <h1>Todo: {params.message}</h1>
-            <h1>IS COMPLETE: {isComplete ? "Yes" : "No"}</h1>
-            <button onClick={()=>completeTask()}>{!isComplete ? "Complete This Task?": "Uncomplete This task"}</button>
+            {!edit ? (
+                <div>
+                    <div className="detail">Todo: {message}</div>
+                    <div className="detail">IS COMPLETE: {isComplete ? "Yes" : "No"}</div>
+                    <button onClick={() => deleteTask()}>Delete</button>
+                    <button onClick={()=>completeTask()}>{!isComplete ? "Complete This Task?": "Uncomplete This task"}</button>
+                    <button onClick={() => setEdit(true)}>Edit</button>
+                </div>
+            ) : (
+                <div>
+                    <div className="detail">
+                        Todo: <input className="edit-detail" type="text" value={message} onChange={(e) => setMessage(e.target.value)}/>
+                    </div>
+                    <div className="detail">IS COMPLETE: {isComplete ? "Yes" : "No"}</div>
+                    <button onClick={() => saveTask()}>Save</button>
+                </div>
+            )}
         </div>
     )
 }
