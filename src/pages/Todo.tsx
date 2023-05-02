514a3f79422af3ca217208792aa96c2c71e12ef4
shawn.thorne9 <shawn.thorne9@gmail.com>
2023-05-01 20:23:38 -0600

    stuff

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,25 +1,40 @@
 import { useContext, useEffect, useState } from "react";
 import { rtdb } from "../lib/firebase";
 import UserContext from "../context/user";
-import { useParams, useSearchParams } from "react-router-dom"
-import { get, ref, child } from "firebase/database";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom"
+import { set, get, ref, child, push, update } from "firebase/database";
 
 export const Todo = () => {
     const params = useParams();
     const user = useContext(UserContext);
+    const navigate = useNavigate();
     const [message, setMessage] = useState();
     const [isComplete, setComplete] = useState();
 
     useEffect(()=>{
-        console.log(params.location)
         get(child(ref(rtdb), `users/${user?.uid}/locations/${params.location}/todos/${params.message}`))
         .then(snapshot =>{
-            console.log(snapshot.val());
+            setMessage(snapshot.val()['message'])
+            setComplete(snapshot.val()['isComplete'])
         })
     },[])
+
+    function completeTask(){
+        set(ref(rtdb,`/users/${user?.uid}/locations/${params.location}/todos/${params.message}`),{
+            isComplete: !isComplete
+        }).then(()=>{
+            navigate('/dashboard');
+        })
+    }
     return (
         <div>
             <h1>Todo: {params.message}</h1>
+            <h1>IS COMPLETE: {isComplete ? "Yes" : "No"}</h1>
+            {!isComplete &&
+                <div>
+                    <button onClick={()=>completeTask()}>Complete This Task?</button>
+                </div>
+            }
         </div>
     )
 }
